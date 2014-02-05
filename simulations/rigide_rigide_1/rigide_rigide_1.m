9e680d3c8fb1db7ea03989ac5a7f1289b3712d09
Mathieu (matael) Gaborit <mat.gaborit@gmx.com>
2014-02-05 23:23:20 +0100

    Ajout de script pour tracer les modes rigide-rigide

--- simulations/rigide_rigide_1/rigide_rigide_1.m
+++ simulations/rigide_rigide_1/rigide_rigide_1.m
@@ -29,10 +29,10 @@ f = 440; % fréquence
 w = 2*pi*f;
 k = w/c;
 
-N = 1;
-A = [1];
+N = 10;
+A = ones(1,N);
 
-L = 5;
+L = 100;
 
 y_vect = 0:0.1:L;
 x_vect = 0:100;
@@ -40,24 +40,35 @@ t_vect = 0:1/(10*f):1/f;
 
 [xx, yy] = meshgrid(x_vect,y_vect);
 
-i = 0;
 
-for t=t_vect
-	ptot = zeros(length(x_vect),length(y_vect));
+% crée une image du champ de pression en y pour chaque mode
+for n_idx=1:N
+	n = n_idx -1; 	% n_idx est utilisé pour les références dans les tableaux
+					% n sert dans les calculs
+	kx = sqrt(k^2-(n*pi/L)^2);
+
+	eigen_fct2d= cos(n*pi/L*y_vect);
+	eigen_fct3d= cos(n*pi/L*yy);
+
+	ptot_abs = A(n_idx)*eigen_fct3d.*abs(exp(-j*kx*xx));
 
 
-	for ix=1:length(x_vect)
-		for iy=1:length(y_vect)
-			for n=1:N
-				kx = sqrt(k^2 - (pi*n/L)^2);
-				ptot(iy,ix) = ptot(ix,iy) + A(n).*cos(pi*n/L*y_vect(iy)).*exp(w*t - kx*x_vect(ix));
-			end
-		end
-	end
-
+	% figures
 	close all;
-	imagesc(xx,yy,ptot);
-	print('-dpng', ['rigiderigide_' num2str(i) '.png']);
+	figure(1);
+	colormap(hot(250));
 
-	i = i+1;
+	subplot(211);
+	plot(eigen_fct2d, y_vect);
+	xlabel('Eigenfunction');
+	ylabel('y');
+	title(['n=' num2str(n)]);
+
+	subplot(212);
+	imagesc(xx, yy, ptot_abs);
+	xlabel('x');
+	ylabel('y');
+
+	% enregistrement
+ 	print('-dpng', ['rigiderigide_' num2str(n) '.png']);
 end
